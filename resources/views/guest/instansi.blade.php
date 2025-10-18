197f4e7076ceee33378b93438a7049b334317c09
Kaila Baim <Kailabaim012@gmail.com>
2025-10-18 07:12:06 -0700

    Skip vite build, use pre-built assets

--- resources/views/guest/instansi.blade.php
+++ resources/views/guest/instansi.blade.php
@@ -5,7 +5,7 @@
   <title>Buku Tamu Instansi - SMKN 13 Bandung</title>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-  <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
+  <link rel="stylesheet" href="/assets/css/styles.css">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
